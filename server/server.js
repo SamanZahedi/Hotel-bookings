33d1f97fabe318c979b78d0f641b25134892008c
Davood Khoshnood <khoshnood1977@gmail.com>
2022-06-20 02:42:09 +0100

    new branch

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-app.use(express.json())
-
-
-const port = process.env.port || 5000
-
-app.listen(port, ()=> console.log(`Server is listening on port ${port}`))
