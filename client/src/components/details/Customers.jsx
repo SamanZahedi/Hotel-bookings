e33b62489096e23ee1125f03934e7ff6ee542f5c
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-06-26 22:06:19 +0100

    Update Customers.jsx

--- client/src/components/details/Customers.jsx
+++ client/src/components/details/Customers.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Customers = () => {
-  return <div>Customers</div>
+  return (
+<div><nav>
+ <input className="form-control" placeHolder="Search..."/>
+<label for="sort" >Sort</label>
+ <input name="sort" id="sort" className="form-control" />
+
+</nav>
+</div>
+
+)
 }
 
 export default Customers
