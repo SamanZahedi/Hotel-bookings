5ada6b8e8176e49b426bdcfd71215c0449b64838
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-06-26 22:13:11 +0100

    Update Customers.jsx

--- client/src/components/details/Customers.jsx
+++ client/src/components/details/Customers.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Customers = () => {
   return (
-<div><nav>
+<main><nav>
  <input className="form-control" placeHolder="Search..."/>
 <label for="sort" >Sort</label>
  <input name="sort" id="sort" className="form-control" />
 <button className="btn btn-success shadow p-2">New</button>
 </nav>
-</div>
+</main>
 
 )
 }
