97c702fe5c71dad310dd5b41d0ee794be5bc550e
Davood Khoshnood <khoshnood1977@gmail.com>
2022-07-01 11:02:56 +0100

    Add modal and edit to customers

--- client/src/components/details/Hotels.jsx
+++ client/src/components/details/Hotels.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-// import AddHotel from "./AddHotel";
-import Search from "./Search";
-import Sort from "./Sort";
-import ModalForm from "./ModalForm";
-import ModalApproved from "./ModalApproved";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Search from './Search'
+import Sort from './Sort'
+import ModalForm from './ModalForm'
+import ModalApproved from './ModalApproved'
 
 const Hotels = () => {
-  const [hotels, setHotels] = useState([]);
-  const [reload, setReload] = useState(true);
-  const [allHotels, setAllHotels] = useState([]);
-
-
+  const [hotels, setHotels] = useState([])
+  const [reload, setReload] = useState(true)
+  const [allHotels, setAllHotels] = useState([])
 
   const loadData = () => {
-    axios.get("https://hotels-bookings.herokuapp.com/hotels").then((res) => {
-      setAllHotels(res.data);
-      setHotels(res.data);
-    });
-  };
+    axios.get('https://hotels-bookings.herokuapp.com/hotels').then((res) => {
+      setAllHotels(res.data)
+      setHotels(res.data)
+    })
+  }
 
   // const loadData = () => {
   //   axios.get("http://localhost:3005/hotels").then((res) => {
@@ -28,22 +25,22 @@ const Hotels = () => {
   // }
 
   useEffect(() => {
-    loadData();
-  }, []);
+    loadData()
+  }, [])
 
   const deleteHandler = (id, approval) => {
-    console.log("delete", approval, id);
+    console.log('delete', approval, id)
     if (approval)
       axios
         .delete(`https://hotels-bookings.herokuapp.com/hotels/${id}`)
         .then(() => {
-          loadData();
-        });
-  };
+          loadData()
+        })
+  }
 
   useEffect(() => {
-    setReload(false);
-  }, [reload]);
+    setReload(false)
+  }, [reload])
 
   return (
     <>
@@ -56,26 +53,24 @@ const Hotels = () => {
       <div className="content">
         {hotels.map((hotel) => (
           <div key={hotel.id} className="card-row">
-            <div key={hotel.id}>
-              <h3>Hotel name:</h3>
-              <span>{hotel.name}</span>
+            <div key={hotel.id} className="d-flex-row-justify-start">
+              <div>
+                <h3>Hotel name:</h3>
+                <span>{hotel.name}</span>
+              </div>
+              <div>
+                <h4>Postcode: {hotel.postcode}</h4>
+                <h5>Number of rooms: {hotel.rooms}</h5>
+              </div>
             </div>
             <div>
-              <h4>Postcode: {hotel.postcode}</h4>
-              <h5>Number of rooms: {hotel.rooms}</h5>
-            </div>
-            <div>
-              <ModalApproved          
-                 deleteHandler={deleteHandler}
-                id={hotel.id}
-              />
-              
+              <ModalApproved deleteHandler={deleteHandler} id={hotel.id} />
             </div>
           </div>
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Hotels;
+export default Hotels
